0d8c1bd4f682c061f201e5ac406fa261e9afe7d5
josefinadev <josefina.gradosreyes@gmail.com>
2026-03-25 15:36:23 -0500

    actualizacion

--- conexion.php
+++ conexion.php
@@ -2,11 +2,11 @@
 
     function conexion(){
 
-    $host = "host=dpg-d723d9h5pdvs73a8cj1g-a.oregon-postgres.render.com/";
+    $host = "host=dpg-d723d9h5pdvs73a8cj1g-a.oregon-postgres.render.com";
     $port = "port=5432";
-    $dbname = "test_7z3w";
-    $user = "test_7z3w_user";
-    $password = "80YHuKSlvTuj36SmiV3MLFe7S8QCTxgP";
+    $dbname = "dbname=test_7z3w";
+    $user = "user=test_7z3w_user";
+    $password = "password=80YHuKSlvTuj36SmiV3MLFe7S8QCTxgP";
 
     $db = pg_connect("$host $port $dbname $user $password");
 
